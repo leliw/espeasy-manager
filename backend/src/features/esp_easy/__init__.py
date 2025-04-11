f77ed5607e16549b192c28d18746ada8f2ffb73e
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-04-11 17:11:35 +0200

    amend

--- backend/src/features/esp_easy/__init__.py
+++ backend/src/features/esp_easy/__init__.py
@@ -1,10 +1,11 @@
 """ESPEasy - A Python library to communicate with ESPEasy devices"""
 
-from .esp_easy_model import NodeInfo
+from .esp_easy_model import NodeInfo, Sensor
 from .udp_receiver import NodeReceiver, UdpReceiver
 
 __all__ = [
     "NodeInfo",
     "NodeReceiver",
     "UdpReceiver",
+    "Sensor"
 ]
